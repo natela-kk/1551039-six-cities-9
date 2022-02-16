4c74ee10ff37f4a0bec4c50e3475b61fc9f2c8f7
Natela <n.kerd@mail.ru>
2022-02-16 03:14:14 +0300

    2.1

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import CardComponent from './components/card-component/card-component';
+
+const offers = {
+  OFFERS_COUNT: 312,
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App offersCount = {offers.OFFERS_COUNT}/>,
   document.getElementById('root'));
+
+ReactDOM.render(
+  <>
+    <CardComponent />,
+    <CardComponent />,
+    <CardComponent />,
+    <CardComponent />,
+    <CardComponent />,
+  </>,
+  document.querySelector('.cities__places-list'));
